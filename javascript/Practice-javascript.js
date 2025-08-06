bc98cb2d7d2a711a28862737d256e65960f04892
web3saiful <web3saiful@gmail.com>
2025-08-06 19:59:01 +0600

    Updated my code

--- javascript/Practice-javascript.js
+++ javascript/Practice-javascript.js
@@ -1,6 +1,9 @@
-var oldSchool="iba"
-var oldSchool="BUET"
-console.log(oldSchool
-);
-const pi=3.14;
-clg.log(pi);
+// todo Arithmatic opperators
+let a =15;
+let b =10;
+console.log(a+b); //addition
+console.log(a-b);//subtraction
+console.log(a*b); //multiplication
+console.log(a/b); //division
+console.log(a%b);//modulus
+console.log(a**b); //exponentiation
