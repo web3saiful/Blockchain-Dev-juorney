f5f6f93ffeae885ee63ff74f536e03a6bebba974
web3saiful <web3saiful@gmail.com>
2025-08-27 19:37:04 +0600

    Updated my code

--- javascript/java.js
+++ javascript/java.js
@@ -1,23 +1,4 @@
-function sayHello() {
-  console.log("Hello from JavaScript!");
-}
+let age =10;
+age=24;
+const name="eshan";
 
-sayHello();
-
-
-let number = 10;
-
-if (number > 5) {
-  console.log("Number is greater than 5");
-} else {
-  console.log("Number is 5 or less");
-}
-
-for (let i = 1; i <= 5; i++) {
-  console.log("Counting: " + i);
-}
-let fruits = ["Apple", "Banana", "Mango"];
-
-console.log(fruits[0]); // Apple
-
-console.log(fruits[1]); // Banana
