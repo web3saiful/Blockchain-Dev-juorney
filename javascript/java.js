5b0f862dae3f262eb17f7517129e9b94ccd7e592
web3saiful <web3saiful@gmail.com>
2025-08-28 09:27:12 +0600

    Updated my code

--- javascript/java.js
+++ javascript/java.js
@@ -1,5 +1,14 @@
-  function say (message="Hello Hablu Programmer"){
+//   function say (message="Hello Hablu Programmer"){
 
-      console.log (message)
-    };
-  say("Eshan How Are You?")
+//       console.log (message)
+//     };
+//   say("Eshan How Are You?")
+
+
+
+
+  var strtype=`uu`;
+  console.log(typeof strtype)
+
+  var numType=66;
+  console.log(typeof numType)
