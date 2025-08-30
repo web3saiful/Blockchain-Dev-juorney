f2947d7d81da86dcaee2ceb4ccff90ed0ee487e2
web3saiful <web3saiful@gmail.com>
2025-08-30 21:21:27 +0600

    Updated my code

--- javascript/java.js
+++ javascript/java.js
@@ -80,8 +80,134 @@
 // Book.fun()
 
 
+// let array =['1','b','c','6']
+// for (let i=0; i<array.length;i++){
+//     console.log(array[i])
+// }
 
-let array =['1','b','c','6']
-for (let i=0; i<array.length;i++){
-    console.log(array[i])
+// let array =['a','b','c','d'];
+// console.dir(array);
+
+
+// let obj={
+    
+//         name:"eshan",
+//         id:2,
+//         profe:"programmer"
+    
+// }
+// // console.dir(obj)
+
+// for(Element of obj){
+//     console.log(Element)
+// }
+
+
+
+
+// let Hublu=[2,3,10]
+// for(let i=0; i<Hublu.length;i++){
+//     console.log(Hablu[])
+
+// }
+
+// let Hublu1=[2,5,10,20]
+// for (ForOf of Hublu1){
+     
+// }
+
+// let Hublu2=[2,5,10,20]
+// Hublu2.forEach(MyFun)
+// function MyFun(z){
+//     console.log(z)
+// }
+
+// map
+// let Hablu=[2,4,6,10];
+// let Hablu1=Hublu.map(myFun)
+
+// function myFun(x){
+//     return x*2
+// }
+// console.log(Hublu)
+
+
+
+// inharitence
+
+// class parent{
+//     Assets1(){
+//         console.log("eta amader dadar assets");
+
+//     }
+//     Assets2(){
+//         console.log("eta amader babar assets");
+//     }
+// }
+
+// class Ami extends parent{
+// //   MyAssets(){
+// //     super.Assets1();
+// //     super.Assets2();
+// //   }
+// }
+
+// let MyAssets=new Ami();
+
+// MyAssets.Assets1();
+// MyAssets.Assets2();
+
+
+
+
+
+
+
+// Map
+// var number =[1,2,3,4,5,5]
+// var result =number.map((num)=>{
+//  return 2*num;
+
+// });
+
+// console.log(result);
+
+
+// filter haublu
+// let Hublu=[2,4,6,8,10];
+// let Hublu1=Hublu.filter(MyFun)
+
+// function Myfun(x){
+//     return x>4
+// }
+// console.log(Hublu1)
+
+
+
+
+
+
+// filter Sumit
+// var number =[1,2,3,4,5,6,7,8,9,10];
+// var result =number.filter((currentValue)=>{
+//     return currentValue >4;
+
+// });
+// console.log(result);
+
+
+
+
+let fun1=()=>{
+    console.log("fun1");
 }
+let fun2=()=>{
+    console.log("fun2");
+}
+let fun3=()=>{
+    console.log("fun3");
+}
+
+fun1()
+fun1()
+fun2()
